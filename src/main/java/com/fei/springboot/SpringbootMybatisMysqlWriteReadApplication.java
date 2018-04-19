82ff93557de1ed32c70196953e92495b95298e3b
ssggithub <13691897490@163.com>
2018-04-19 12:47:08 +0800

    spring+mysql读写分离

---
+++ src/main/java/com/fei/springboot/SpringbootMybatisMysqlWriteReadApplication.java
@@ -0,0 +1,12 @@
+package com.fei.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisMysqlWriteReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisMysqlWriteReadApplication.class, args);
+	}
+}
